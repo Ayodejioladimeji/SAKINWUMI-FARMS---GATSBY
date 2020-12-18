69ee847a2873517603e212be9ec77e70a0de6389
Ayodejioladimeji <layobright11@gmail.com>
2020-12-18 07:25:14 -0800

    new update added

--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import cock from '../assets/cock.jpg'
+// import cock from '../assets/cock.jpg'
 import {Link} from 'gatsby'
 
 const Two = () => {
     return (
-        <div className="two" style={{ backgroundImage:`url(${cock})` }}>
+        <div className="two">
             <div className="two-cover">
                <div className="two-center">
-                   <h1>CARE AT EVERY STAGE</h1>
-                   <h3>Our poultry receives great attendance and care throughout every stage of its development due to advanced technologies.</h3>
+                   <h1 data-aos="fade-right">CARE AT EVERY STAGE</h1>
+                   <h3 data-aos="fade-left">Our poultry receives great attendance and care throughout every stage of its development due to advanced technologies.</h3>
                     <Link to="/about">
-                        <button className="two-button">READ MORE</button>
+                        <button className="two-button" data-aos="fade-up">READ MORE</button>
                     </Link>
                </div>
             </div>
