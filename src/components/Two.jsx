c4e3623ba6c83853cea7ddff79e7bc11e17ac224
Ayodejioladimeji <layobright11@gmail.com>
2020-12-18 09:20:41 -0800

    changes made

--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -7,10 +7,10 @@ const Two = () => {
         <div className="two">
             <div className="two-cover">
                <div className="two-center">
-                   <h1 data-aos="fade-right">CARE AT EVERY STAGE</h1>
-                   <h3 data-aos="fade-down">Our poultry receives great attendance and care throughout every stage of its development due to advanced technologies.</h3>
+                   <h1>CARE AT EVERY STAGE</h1>
+                   <h3>Our poultry receives great attendance and care throughout every stage of its development due to advanced technologies.</h3>
                     <Link to="/about">
-                        <button className="two-button" data-aos="fade-up">READ MORE</button>
+                        <button className="two-button">READ MORE</button>
                     </Link>
                </div>
             </div>
