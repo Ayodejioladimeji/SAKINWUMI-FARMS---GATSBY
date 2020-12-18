55d55709e81380b30b0b176ba87999cfdba2da32
Ayodejioladimeji <layobright11@gmail.com>
2020-12-18 10:43:37 -0800

    changes made

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -12,24 +12,25 @@ const Three = () => {
                     {table.map((data)=>{
                         const {id, icon, title, text} = data
                         return(
-                            <div className="table-one" key={id}>
+                            <div className="table-one" key={id} data-aos="flip-right">
                                 {icon}
                                 <div className="table-words">
-                                    <h2>{title}</h2>
-                                    <p>{text}</p>
+                                    <h2 data-aos="fade-down">{title}</h2>
+                                    <p data-aos="fade-up">{text}</p>
                                 </div>
                             </div>
                         )
                     })}
                 </div>
                 <div className="three-center">
-                   <img src={sprout} alt="sprout"/>
-                   <p>SAKINWUMI FARMS</p>
+                   <img src={sprout} alt="sprout" data-aos="fade-down"/>
+                   <p data-aos="fade-right">SAKINWUMI FARMS</p>
 
-                   <div className="three-text">
+                   <div className="three-text" data-aos="zoom-in">
                         <h1>Agriculture Is Providing The Best Solution of Eco Environment</h1>
                    </div>
-                   <div className="three-buttons">
+                   
+                   <div className="three-buttons" data-aos="fade-up">
                         <Link to="/services">
                             <button className="btn-two">SERVICES</button>
                         </Link>
