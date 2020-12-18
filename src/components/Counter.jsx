e8eaeec8cf873bf836d303c7d58f418a65def10e
Ayodejioladimeji <layobright11@gmail.com>
2020-12-18 10:00:56 -0800

    changes made

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,9 +8,9 @@ const Counter = () => {
                 {counter.map((data)=>{
                     const {id, icon, title, text} = data
                     return(
-                        <div className="count-one" key={id}>
-                            <div className="div-icon">{icon}</div>
-                            <div className="count-words">
+                        <div className="count-one" key={id} data-aos="zoom-out">
+                            <div className="div-icon" fade-right>{icon}</div>
+                            <div className="count-words" data-aos="fade-up">
                                 <h2>{title}</h2>
                                 <p>{text}</p>
                             </div>
